7851f518c9dc8e9aad0a8cbab68d968bb2b74661
vagrant <vagrant@homestead>
2018-01-03 07:37:43 +0000

    finish user auth

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//主页
 Route::get('/', 'PagesController@root')->name('root');
 
+//系统自动生成
+//Auth::routes();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//不需要这个主页
+//Route::get('/home', 'HomeController@index')->name('home');
