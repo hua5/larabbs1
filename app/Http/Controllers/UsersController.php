3ed01106f17be30c52bb65da59ca6736e6273427
vagrant <vagrant@homestead>
2018-01-03 10:07:16 +0000

    basic user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
-{
+{   //用户显示页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+    //用户编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+  //编辑好后 更新跳转页面
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
