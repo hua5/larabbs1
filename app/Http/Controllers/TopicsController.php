5be4de0298818f196c174da7ebdb0e6b34d5c3fa
vagrant <vagrant@homestead>
2018-01-05 11:07:30 +0000

    topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,10 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
 	//	$topics = Topic::paginate(); 优化后 直接关联 用户 和分类  实行预加载
-		$topics = Topic::with('user', 'category')->paginate(30);
+	//	$topics = Topic::with('user', 'category')->paginate(30);
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
