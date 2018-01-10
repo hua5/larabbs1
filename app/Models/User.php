61efe7609a6de29d1635ea42936f7db5f773728e
vagrant <vagrant@homestead>
2018-01-10 07:25:53 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,13 @@ class User extends Authenticatable
         return $this->hasMany(Topic::class);
     }
 
+    //一个用户可以有很多回复
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
