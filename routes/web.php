eb28e6bf84734cca5ca4dc649e5c2bc3e77e7029
vagrant <vagrant@homestead>
2018-01-11 13:06:55 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 //主页
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //系统自动生成
 //Auth::routes();
