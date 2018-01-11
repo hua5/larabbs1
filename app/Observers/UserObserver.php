955bb752d61afe4b0beb9e30d7930f9c579db9fd
vagrant <vagrant@homestead>
2018-01-11 13:09:55 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,13 +9,12 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+//我们将使用模型监控器来实现此功能，在用户数据即将入库之前，将为 avatar 字段设置一张默认的头像 ：
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
