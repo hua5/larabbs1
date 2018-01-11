93765048eb3670eb58a8d33b405fa45a2789d5df
vagrant <vagrant@homestead>
2018-01-11 07:23:11 +0000

    add the new user admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,31 @@ class User extends Authenticatable
         //dd( $this->unreadNotifications);
         $this->unreadNotifications->markAsRead();
     }
+
+//设置了一个修改器  当我们尝试在模型上设置 password 的值时，该修改器将被自动调用：
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+//设置了一个修改器  当我们尝试在模型上设置 Avatar 的值时，该修改器将被自动调用：
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
+
 }
